c62c6a26298e09cebb55ebde83a8574888d88db6
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:41:38 -0500

    feat: :sparkles: exercise2 comparison operators definitions of the second types of operators together with a  multiple examples of its implementation and explicative comments.

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -1,123 +1,25 @@
-let variable= "valor"
+DefinicionOperadoresComparacion="Un operador de comparación lleva a cabo una comparación entre sus operandos y devuelve un valor lógico según el resultado de la comparación si fue verdadera (true) o falsa (false). Los operandos pueden ser valores numéricos, de cadena, lógicos u objetos. Casi siempre si los dos operandos no son del mismo tipo, JavaScript intenta dinámicamente convertirlos a un tipo apropiado para la comparación"
+//Dados los siguientes números:
+const primerOperando = 13
+const segundOperando = "13"
+const tercerOperando = 31
+const cuartOperando = 13
+// ==Devuelve true si los valores de los operandos son iguales y false si no lo son.
+console.log(primerOperando==tercerOperando);
+console.log(primerOperando==segundOperando);
+console.log(primerOperando==cuartOperando);
 
-asignacion="Un operador de asignación asigna un valor a su operando izquierdo basándose en el valor de su operando derecho."
-a= asignacion
-console.log("Valor de la variable 'a' basado en el valor previo de 'asignacion': ", a)
+//Devuelve true si los valores y el tipo de dato de los operandos son iguales y false si no lo son.
+console.log(primerOperando===segundOperando)
+console.log(primerOperando==cuartOperando)
+// > < Devuelven true o false segun la operacion
+console.log(primerOperando>tercerOperando)
+console.log(primerOperando<tercerOperando)
+console.log(primerOperando<cuartOperando)
+console.log(primerOperando<=cuartOperando)
 
-
-valor2= 2
-valor1=3
-//Assignment	
-valor1 = valor2	
-//which is equal to
-//valor1 = valor2
-
-console.log(valor1)
-
-//Addition assignment	
-valor1 += valor2	
-//which is equal to
-//valor1 = valor1 + valor2
-
-console.log(valor1)
-
-//Subtraction assignment	
-valor1 -= valor2	
-//which is equal to
-//valor1 = valor1 - valor2
-
-console.log(valor1)
-
-//Multiplication assignment	
-valor1 *= valor2	
-//which is equal to
-//valor1 = valor1 * valor2
-
-console.log(valor1)
-
-//Division assignment	
-valor1 /= valor2	
-//which is equal to
-//valor1 = valor1 / valor2
-
-console.log(valor1)
-
-//Remainder assignment	
-valor1 %= valor2	
-//which is equal to
-//valor1 = valor1 % valor2
-
-console.log(valor1)
-
-//Exponentiation assignment	
-valor1 **= valor2	
-//which is equal to
-//valor1 = valor1 ** valor2
-
-console.log(valor1)
-
-//Let shift assignment	
-valor1 <<= valor2	
-//which is equal to
-//valor1 = valor1 << valor2
-
-console.log(valor1)
-
-//Right shift assignment	
-valor1 >>= valor2	
-//which is equal to
-//valor1 = valor1 >> valor2
-
-console.log(valor1)
-
-//Unsigned right shift assignment	
-valor1 >>>= valor2	
-//which is equal to
-//valor1 = valor1 >>> valor2
-
-console.log(valor1)
-
-//Bitwise AND assignment	
-valor1 &= valor2	
-//which is equal to
-//valor1 = valor1 & valor2
-
-console.log(valor1)
-
-//Bitwise valor1OR assignment	
-valor1 ^= valor2	
-//which is equal to
-//valor1 = valor1 ^ valor2
-
-console.log(valor1)
-
-//Bitwise OR assignment	
-valor1 |= valor2	
-//which is equal to
-//valor1 = valor1 | valor2
-
-console.log(valor1)
-
-//Logical AND assignment	
-valor1 &&= valor2	
-//which is equal to
-//valor1 && (valor1 = valor2)
-
-console.log(valor1)
-
-//Logical OR assignment	
-
-
-valor1 ||= valor2	
-//which is equal to
-//valor1|| (valor1 = valor2)
-
-console.log(valor1)
-
- //Nullish coalescing assignment	
-valor1 ??= valor2	
-//which is equal to
-//
-valor1 ?? (valor1 = valor2)
-
-console.log(valor1)
+// ! vuelve false en true y true en false, es decir devuelve el lo opuesto a lo que es añadido. Tambien se puede usar para que en una igualdad si son iguales arroje false y si no lo son, en lugar de arrojar false de true, covirtiendose así en un operador de 'desigualdad'
+console.log(primerOperando!=cuartOperando)
+console.log(primerOperando==segundOperando);
+console.log(primerOperando!==segundOperando);
+// ! == es el contrario de === y no de ==, que es solo es !=.
