0326cc7f9ca7c67ae7520a364166960be47ddcde
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:38:24 -0500

    feat: :sparkles: exercise1.1 add assignment operators i found more examples of this kind ok operator...

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -3,3 +3,121 @@ let variable= "valor"
 asignacion="Un operador de asignación asigna un valor a su operando izquierdo basándose en el valor de su operando derecho."
 a= asignacion
 console.log("Valor de la variable 'a' basado en el valor previo de 'asignacion': ", a)
+
+
+valor2= 2
+valor1=3
+//Assignment	
+valor1 = valor2	
+//which is equal to
+//valor1 = valor2
+
+console.log(valor1)
+
+//Addition assignment	
+valor1 += valor2	
+//which is equal to
+//valor1 = valor1 + valor2
+
+console.log(valor1)
+
+//Subtraction assignment	
+valor1 -= valor2	
+//which is equal to
+//valor1 = valor1 - valor2
+
+console.log(valor1)
+
+//Multiplication assignment	
+valor1 *= valor2	
+//which is equal to
+//valor1 = valor1 * valor2
+
+console.log(valor1)
+
+//Division assignment	
+valor1 /= valor2	
+//which is equal to
+//valor1 = valor1 / valor2
+
+console.log(valor1)
+
+//Remainder assignment	
+valor1 %= valor2	
+//which is equal to
+//valor1 = valor1 % valor2
+
+console.log(valor1)
+
+//Exponentiation assignment	
+valor1 **= valor2	
+//which is equal to
+//valor1 = valor1 ** valor2
+
+console.log(valor1)
+
+//Let shift assignment	
+valor1 <<= valor2	
+//which is equal to
+//valor1 = valor1 << valor2
+
+console.log(valor1)
+
+//Right shift assignment	
+valor1 >>= valor2	
+//which is equal to
+//valor1 = valor1 >> valor2
+
+console.log(valor1)
+
+//Unsigned right shift assignment	
+valor1 >>>= valor2	
+//which is equal to
+//valor1 = valor1 >>> valor2
+
+console.log(valor1)
+
+//Bitwise AND assignment	
+valor1 &= valor2	
+//which is equal to
+//valor1 = valor1 & valor2
+
+console.log(valor1)
+
+//Bitwise valor1OR assignment	
+valor1 ^= valor2	
+//which is equal to
+//valor1 = valor1 ^ valor2
+
+console.log(valor1)
+
+//Bitwise OR assignment	
+valor1 |= valor2	
+//which is equal to
+//valor1 = valor1 | valor2
+
+console.log(valor1)
+
+//Logical AND assignment	
+valor1 &&= valor2	
+//which is equal to
+//valor1 && (valor1 = valor2)
+
+console.log(valor1)
+
+//Logical OR assignment	
+
+
+valor1 ||= valor2	
+//which is equal to
+//valor1|| (valor1 = valor2)
+
+console.log(valor1)
+
+ //Nullish coalescing assignment	
+valor1 ??= valor2	
+//which is equal to
+//
+valor1 ?? (valor1 = valor2)
+
+console.log(valor1)
