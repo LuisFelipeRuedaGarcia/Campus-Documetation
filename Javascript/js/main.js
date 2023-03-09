72ff09e26c2a4f0760442956675adfba1e9962eb
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:04:32 -0500

    feat: :sparkles: exercise8 repeat method The eighth string method is explained, for this we use a variable named with the name of the method to which a string is assigned by value with the explanation of said method. then the string is printed in the console applying that method to be able to visualize the result it returns.

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -1,4 +1,3 @@
-//seventh method
-substringMethod = "Devuelve un subconjunto de un objeto String. If o startPositionis greater than o endPosition:The substring() method swaps its functions: the startPositionbecomes to the endPosition and vice versa; While the slice() method returns an empty string. The slice() method allows the use of negative values ​​in startPositionand endPosition. In this case, the position starts counting from the end of the string."
-console.log(substringMethod.substring(12, 109))
-console.log(substringMethod)
+//eighth method
+repeatMethod = "El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de copias de la cadena en la cual fue llamada, concatenados."
+console.log(repeatMethod.repeat(12))
