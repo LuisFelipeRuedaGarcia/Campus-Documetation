fb6ab16467e7c8f920bc6918889a55bdd961dfbf
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:49:49 -0500

    feat: :sparkles: exercise5 Math library the methods and properties of the 'math' library or object are added to the list of operators, they are definite and accompanied by  examples of its implementation and explicative comments.

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -1,50 +1,36 @@
-//Un operador aritmético toma valores numéricos (ya sean literales o variables) como sus operandos y devuelve un solo valor numérico. 
+// math object
+number=12.5
+//Static properties
 
-numeroa= 6;
-numerob = 9;
+//Ratio of a circle's circumference to its diameter
+console.log(Math.PI)
 
+//Methods
+//To fixed formats a number using fixed-point notation.
+let numberFixed = 12.12345
+function financial(x) {
+    return Number.parseFloat(x).toFixed(2);
+}
+console.log(financial(numberFixed))
+console.log()
+//Max Returns the largest of zero or more numbers.
+console.log(Math.max(number,3))
+//Min Returns the smallest of zero or more numbers.
+console.log(Math.min(number,3))
+//Pow Returns base x to the exponent power y.
+console.log(Math.pow(number,2))
+//Random Returns a pseudo-random number between 0 and 1
+console.log(Math.random())
+//Sqrt Returns the positive square root of number
+console.log(Math.sqrt(number))
 
-//SUMA
-resultadoSuma = numeroa + numerob;
-
-//RESTA
-resultadoResta = numeroa - numerob;
-
-//DIVISION
-resultadoDivisión = numeroa / numerob;
-
-//MULTIPLICACION.
-resultadoMultiplicación = numeroa * numerob;
-
-//Potenciaciación
-resultadoPotenciación = numeroa ** numerob;
-
-
-//Residuo "residuo de una división % "
-resultadoResiduo = numerob%numeroa
-
-console.log("Resultado de la suma", resultadoSuma);
-console. log("Resultado de la Resta:",resultadoResta);
-console.log("Resultado de la División:",resultadoDivisión);
-console. log("Resultado de la Multiplicación: ",resultadoMultiplicación);
-console.log("Resultado de la Potenciación" , resultadoPotenciación)
-
-console.log("Resultado del Residuo", resultadoResiduo)
-
-console.log(numeroa)
-//Incremento, suma 1
-numeroa++
-console.log(numeroa)
-
-console.log(numeroa)
-//Decremento, resta 1
-numeroa--
-console.log(numeroa)
-
-//Unary negative operator. Returns the negation of its operand.
-console.log(-numeroa)
-
-//Unary plus operator. Attempts to convert the operand to a number, if it is not already.
-console.log(+numeroa)
-console.log(+4)
-console.log(+true)
+console.log(Math.floor(Math.random() * 10))
+//Round methods
+//Ceil Returns the smallest integer greater than or equal to number
+console.log(Math.ceil(number))
+//Floor Returns the largest integer less than or equal to number
+console.log(Math.floor(number))
+//Round Returns the value of the number 'number' rounded to the nearest integer.
+console.log(Math.round(number))
+//Trunc Returns the integer portion of 'number', removing any fractional digits.
+console.log(Math.trunc(number))
