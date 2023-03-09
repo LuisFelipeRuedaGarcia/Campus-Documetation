8c4b56cbfbe92364b1871ec10df2a0fe1a4bf884
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:15:41 -0500

    feat: :sparkles: exercise12 repeat method The twelfth string method is explained, for this we use a variable named with the name of the method to which a string is assigned by value with the explanation of said method. then the string is printed in the console applying that method to be able to visualize the result it returns.

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -1,3 +1,12 @@
-//eleventh method
-lastIndexOfMethod="find substr index, Devuelve la primera posición del texto text."
-console.log(lastIndexOfMethod.lastIndexOf('a', 36))
+//twelfth method
+substrmethod="fEl método substring(start, end) devuelve un  con el fragmento de texto desde la posición start hasta la posición end. Si se omite el parámetro end, el subtexto abarcará desde start hasta el final. El método substr(start, size) devuelve un  con el fragmento de texto desde la posición start, hasta la posición start+size. Dicho de otra forma, size será el tamaño del  resultante. En el caso de omitirse el parámetro size, se devuelve hasta el final."
+console.log(substrmethod.substr('a', 36))
+//another example
+const text = "Submarino";
+
+console.log(text.substring(3));    // 'marino' (desde el 3 en adelante)
+console.log(text.substring(3, 5)); // 'ma'     (desde el 3, hasta el 5)
+console.log(text.substr(3));      // 'marino' (desde el 3 en adelante)
+console.log(text.substr('a', 5));    // 'marin'  (desde el 3, hasta el 3+5)
+console.log(text.substr(-3));      // 'ino'    (desde la posición 3 desde el final, en adelante)
+console.log(text.substr(-3, 2));   // 'in'     (desde la posición 3 desde el final, hasta 2 posiciones más)
