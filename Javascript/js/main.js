ce7ec1ba9f34802440a280179d7bbe92f86e1749
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:23:30 -0500

    feat: :sparkles: exercise15 match method The fifteenth string method is explained, for this we use a variable named with the name of the method to which a string is assigned by value with the explanation of said method. then the string is printed in the console applying that method to be able to visualize the result it returns.

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -1,3 +1,3 @@
-//fourteenth method
-searchMethod="Busca un patrón que encaje con regexp y devuelve la  posición encontrada."
-console.log(searchMethod.search("que"))
+//fifteenth method
+matchMethod="Busca un patrón que encaje con regexp y devuelve un Array con las coincidencias encontradas. null si no se encuentran. El método .matchAll() devuelve un iterador para poder recorrer las coincidencias encontradas."
+console.log(matchMethod.match("que"))
