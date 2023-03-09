c944d3aa5e728b2117d1e09f00af404be4cdb22d
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:45:38 -0500

    feat: :sparkles: exercise4 arithmetic operators definitions of the third types of operators together with a multiple examples of its implementation and explicative comments.

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -1,20 +1,50 @@
-DefinicionOperadoreslogicos="Los operadores lógicos se utilizan normalmente con valores booleanos (lógicos); cuando lo son, devuelve un valor booleano. Sin embargo, los operadores && y ||en realidad devuelven el valor de uno de los operandos especificados, por lo que si estos operadores se utilizan con valores no booleanos, pueden devolver un valor no booleano. Los operadores lógicos se describen en la siguiente tabla."
+//Un operador aritmético toma valores numéricos (ya sean literales o variables) como sus operandos y devuelve un solo valor numérico. 
 
-And = "&&"
-//cuando por ejemplo queremos colocar más de un condicional en un if con este operador se deben cumplir ambas (thefirst AND the second) condiciones para que arroge true y se ejecute, basta con que uno solo de sus valores no sea verdadero para que arroje false.
-console.log(7<8&&6<7)
-console.log(7>8&&6<7)
-console.log(7<8&&6>7)
-console.log(7>8&&6>7)
+numeroa= 6;
+numerob = 9;
 
-Or= "||"
-//a diferencia del anterior con que uno solo de sus valores sea verdadero(thefirst OR thesecond), arroja true.
-console.log(7<8||6<7)
-console.log(7>8||6<7)
-console.log(7<8||6>7)
-console.log(7>8||6>7)
 
-Not="!"
-//como ya explique ! vuelve false en true y true en false, es decir devuelve el lo opuesto a lo que es añadido.
-console.log(!false)
-console.log(!3==3)
+//SUMA
+resultadoSuma = numeroa + numerob;
+
+//RESTA
+resultadoResta = numeroa - numerob;
+
+//DIVISION
+resultadoDivisión = numeroa / numerob;
+
+//MULTIPLICACION.
+resultadoMultiplicación = numeroa * numerob;
+
+//Potenciaciación
+resultadoPotenciación = numeroa ** numerob;
+
+
+//Residuo "residuo de una división % "
+resultadoResiduo = numerob%numeroa
+
+console.log("Resultado de la suma", resultadoSuma);
+console. log("Resultado de la Resta:",resultadoResta);
+console.log("Resultado de la División:",resultadoDivisión);
+console. log("Resultado de la Multiplicación: ",resultadoMultiplicación);
+console.log("Resultado de la Potenciación" , resultadoPotenciación)
+
+console.log("Resultado del Residuo", resultadoResiduo)
+
+console.log(numeroa)
+//Incremento, suma 1
+numeroa++
+console.log(numeroa)
+
+console.log(numeroa)
+//Decremento, resta 1
+numeroa--
+console.log(numeroa)
+
+//Unary negative operator. Returns the negation of its operand.
+console.log(-numeroa)
+
+//Unary plus operator. Attempts to convert the operand to a number, if it is not already.
+console.log(+numeroa)
+console.log(+4)
+console.log(+true)
