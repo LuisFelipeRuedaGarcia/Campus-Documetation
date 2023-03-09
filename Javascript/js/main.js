e16701a190efadc9e21044f3d041a4091845eaef
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 16:32:13 -0500

    feat: :rocket: exercise1 assignment operators definition of the first type of operator together with a short example of its implementation.

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -1,1 +1,5 @@
-//
+let variable= "valor"
+
+asignacion="Un operador de asignación asigna un valor a su operando izquierdo basándose en el valor de su operando derecho."
+a= asignacion
+console.log("Valor de la variable 'a' basado en el valor previo de 'asignacion': ", a)
